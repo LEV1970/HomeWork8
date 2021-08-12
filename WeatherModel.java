8f8e8e1c1fc81e46f43fe441bc11f8038965befb
LEV1970 <80787604+LEV1970@users.noreply.github.com>
2021-08-12 17:03:59 +0300

    Add files via upload

---
+++ WeatherModel.java
@@ -0,0 +1,13 @@
+package HomeWork8;
+
+import HomeWork8.entity.Weather;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface WeatherModel {
+    void getWeather(String selectedCity, Period period) throws IOException;
+
+    public List<Weather> getSavedToDBWeather();
+}
+
